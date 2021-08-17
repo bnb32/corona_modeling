5b034a28b9f4f5935bc3b1e63252fa0de9515033
Brandon N Benton <bnb32@cornell.edu>
2021-08-16 21:35:32 -0700

    Update environment.py

--- sir/environment.py
+++ sir/environment.py
@@ -1,5 +1,4 @@
-#API_KEY='AIzaSyAdWz_SegPgJPe1nC6vgJ09ojSxB_KfaOQ'
-api_key='AIzaSyAP2OeLuXst4yOAfK8ayKoeslE4GXUeNsg'
+api_key=''
 
 csv_pop_file='../../web/2018_state_county_pop.csv'
 population_file='../../web/2018_state_county_pop.json'
