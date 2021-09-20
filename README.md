81a34943dbdd9b31fc8d6c39a62c59c9548fa997
EC2 Default User <bnb32@cornell.edu>
2021-09-20 18:01:00 +0000

    spatial sir and 1d models all working

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 
 Run `pip install -e .`
 
+All models are in covid.models.
+
+For example: `from covid.models import Spatial_SIR`
+
+Instantiate model `spatial_sir = Spatial_SIR()` and intialize using `spatial_sir.initialize(<initial_values_dictionary>)`
+
+Run model using `output = spatial_sir.run_model(<n_days>)`
+
+Plot output of specific compartment with `output[compartment_name]`
+
+Can change model parameters with `spatial_sir.update_parameters(<params>)`
--- covid/models/__init__.py
+++ covid/models/__init__.py
@@ -1,30 +1,44 @@
-from covid.misc import get_logger, laplacian, cases_to_intercepts, cases_to_beta, vec_diff, get_beta_ramp, cost_func
-import covid.fetch as fetch
+from covid.misc import laplacian
+#, cases_to_intercepts, cases_to_beta, vec_diff, get_beta_ramp, cost_func
+#import covid.fetch as fetch
 
 import numpy as np
 
-logger = get_logger()
-parallel_execute=False
+#logger = get_logger()
+#parallel_execute=False
 #parallel_execute=True
 
 class params:
     pass
 
 class CompartmentalModel:
-    def __init__(self,compartment_names):
+    def __init__(self,compartment_names,params=None):
         self.compartment_number = len(compartment_names)
-        self.transfer_matrix = np.zeros((self.compartment_number,self.compartment_number))
+        self.transfer_matrix = np.zeros((self.compartment_number,self.compartment_number),dtype=object)
         self.compartment_names = {compartment_names[i]:i for i in range(self.compartment_number)}
         self.compartment_index = {i:compartment_names[i] for i in range(self.compartment_number)}
-        self.compartment_series = [[0] for i in range(self.compartment_number)]
-        self.params = params()
-        self.params.n_substeps = 20
-        self.params.dt = 1.0/self.params.n_substeps
-        self.params.running_update = False
+        self.compartment_series = {}
 
-    def update_parameters(self):
+        if params is None:
+            self.params = params()
+            self.params.n_substeps = 20
+            self.params.dt = 1.0/self.params.n_substeps
+        else:
+            self.params = params
+
+        self.define_parameters()
+        self.define_transfer_matrix()
+
+    def define_parameters(self):
         return
+
+    def update_parameters(self,params):
+        self.params = params
+        self.define_transfer_matrix():
     
+    def define_transfer_matrix(self):
+        return
+
     def update_transfer_matrix(self,last_compartments):
         return
 
@@ -33,7 +47,7 @@ class CompartmentalModel:
                 'deaths':[],
                 'infected':[],
                 'hospitalized':[],
-                'recovered':[]
+                'recovered':[]}
 
     def store_data(self,data):
         self.data = data
@@ -51,22 +65,27 @@ class CompartmentalModel:
                              self.compartment_names[from_compartment]] = value
 
     def last_compartment_value(self,compartment):
-        return self.compartment_series[-1][self.compartment_names[compartment]]
+        return self.compartment_series[compartment][-1]
 
     def initialize(self,initial_values):
-        self.compartment_series = [initial_values]
+        for name in self.compartment_names:
+            self.compartment_series[name] = [initial_values[name]]
 
     def model_eqns(self,last_compartments):
         
         self.update_transfer_matrix(last_compartments)
 
-        return np.dot(self.transfer_matrix,last_compartments)*self.params.dt
+        next_compartments = []
+        for i in range(self.compartment_number):
+            entry = 0
+            for j in range(self.compartment_number):
+                entry += self.transfer_matrix[i,j]*last_compartments[j]
+            next_compartments.append(entry*self.params.dt)
+
+        return next_compartments
         
     def rk_step(self,last_compartments):
 
-        if self.params.running_update:
-            self.update_parameters()
-        
         rk_step=[0.5,0.5,1,0]
         tmp = [x for x in last_compartments]
         compartments_k = [[] for i in range(self.compartment_number)]
@@ -76,39 +95,32 @@ class CompartmentalModel:
             tmp = self.model_eqns(tmp)
             for i in range(self.compartment_number):
                 compartments_k[i].append(tmp[i])
-            tmp = np.multiply(tmp,rk_step[t]) + np.array(last_compartments)
+            tmp = np.multiply(tmp,rk_step[t]) + last_compartments
 
         stencil=[1,2,2,1]
-        next_compartments = []
-        for i in range(self.compartment_number):
-            entry = last_compartments[i] + 1.0/6.0*np.dot(compartments_k[i],stencil)
-            if entry < 0.0: entry=0.0
-            next_compartments.append(entry)
+        next_compartments = last_compartments + 1.0/6.0*np.tensordot(compartments_k,stencil,axes=(1,0))
+        next_compartments[next_compartments < 0]=0
     
         return next_compartments
 
     def run_model(self,n_days):
         
-        last_compartments = self.compartment_series[0]
+        last_compartments = [self.compartment_series[name][0] for name in self.compartment_names]
         for day in range(n_days):
             for t in range(self.params.n_substeps):
                 last_compartments=self.rk_step(last_compartments)
-                self.compartment_series.append(last_compartments)
+                for i,name in enumerate(self.compartment_names):
+                    self.compartment_series[name].append(last_compartments[i])
 
-        self.results = {}
-        for i in range(self.compartment_number):
-            self.results[self.compartment_index[i]] = np.array([self.compartment_series[t][i] for t in range(len(self.compartment_series))])
-        
-        return self.results
+        return self.compartment_series
 
-class SIR_general(CompartmentalModel):
+class SIR(CompartmentalModel):
     def __init__(self):
         super().__init__(['S','I','R'])
+    
+    def define_transfer_matrix(self):
         
         self.params.beta = 3*14.0
-        self.params.state = 'Washington'
-        self.params.county = None
-        self.params.n_days = 10
 
         self.params.I_to_R_rate = 1.0/14.0
         
@@ -123,9 +135,88 @@ class SIR_general(CompartmentalModel):
         self.update_transfer_value(-alpha,'S','S') 
         self.update_transfer_value(alpha,'I','S')
 
-class SEAIQHRD_general(CompartmentalModel):
+class Spatial_SIR(CompartmentalModel):
+    def __init__(self):
+        super().__init__(['S','I','R'])
+        
+    def define_parameters(self):
+
+        self.params.beta = 3*14.0
+        
+        self.params.grid_size_x = 20
+        self.params.grid_size_y = 20
+        self.params.dx = 1
+        self.params.dy = 1
+        self.params.nx = int(self.params.grid_size_x/self.params.dx)
+        self.params.ny = int(self.params.grid_size_y/self.params.dy)
+        self.params.r0 = 1.0
+
+    def define_transfer_matrix(self):
+
+        self.params.I_to_R_rate = 1.0/14.0
+        
+        self.update_transfer_value(-self.params.I_to_R_rate,'I','I')
+        self.update_transfer_value(self.params.I_to_R_rate,'R','I')
+
+    def update_transfer_matrix(self,last_compartments):
+
+        self.params.N = sum(last_compartments)
+        alpha = self.params.beta/self.params.N*(last_compartments[self.compartment_names['I']]+
+                self.params.r0**2/8.0*laplacian(last_compartments[self.compartment_names['I']],self.params.dx,self.params.dy))
+        
+        self.update_transfer_value(-alpha,'S','S') 
+        self.update_transfer_value(alpha,'I','S')
+
+    def example(self):
+
+        S = np.zeros((self.params.nx,self.params.ny))
+        I = np.zeros((self.params.nx,self.params.ny))
+        R = np.zeros((self.params.nx,self.params.ny))
+        
+        S[:,:] = 10
+        midx = self.params.nx//2
+        midy = self.params.ny//2
+        I[midx-2:midx+2,midy-2:midy+2] = 3
+        
+        self.compartment_series['S'] = [S]
+        self.compartment_series['I'] = [I]
+        self.compartment_series['R'] = [R]
+    
+    def initialize(self,initial_values=None):
+        
+        if initial_values is None:
+            self.example()
+
+class SEIR(CompartmentalModel):
+    def __init__(self):
+        super().__init__(['S','E','I','R'])
+    
+    def define_transfer_matrix(self):
+
+        self.params.beta = 3*14.0
+
+        self.params.E_to_I_rate = 1.0/7.0
+        self.params.I_to_R_rate = 1.0/7.0
+
+        self.update_transfer_value(-self.params.E_to_I_rate,'E','E')
+        self.update_transfer_value(self.params.E_to_I_rate,'I','E')
+        self.update_transfer_value(-self.params.I_to_R_rate,'I','I')
+        self.update_transfer_value(self.params.I_to_R_rate,'R','I')
+
+    def update_transfer_matrix(self,last_compartments):
+
+        self.params.N = sum(last_compartments)
+        alpha = self.params.beta*last_compartments[self.compartment_names['I']]/self.params.N
+        
+        self.update_transfer_value(-alpha,'S','S') 
+        self.update_transfer_value(alpha,'E','S')
+
+class SEAIQHRD(CompartmentalModel):
     def __init__(self):
         super().__init__(['S','E','A','I','Q','H','R','D'])
+        
+    def define_transfer_matrix(self):
+
         self.params.beta = 3*14.0
         
         self.params.E_to_A_rate = 1.0/4.0
