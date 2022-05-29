1ba60402424ea525c99876a81b5d01ed30a73918
bnb32 <b.benton19@gmail.com>
2022-05-29 09:25:09 -0700

    moved argparse func

--- covid/__init__.py
+++ covid/__init__.py
@@ -1,1 +1,13 @@
 """Covid module"""
+import argparse
+
+def covid_argparse():
+    """Parse args for running compartmental model"""
+    parser = argparse.ArgumentParser(description='Plot SIRV Model')
+    parser.add_argument('-state', default='Washington')
+    parser.add_argument('-fit_compartments', default=['S', 'I', 'R', 'V'])
+    parser.add_argument('-plot_compartment', default='I')
+    parser.add_argument('-sim_days', default=100, type=int)
+    parser.add_argument('-data_days', default=100, type=int)
+    parser.add_argument('-out_file', type=str)
+    return parser
--- covid/__main__.py
+++ covid/__main__.py
@@ -1,29 +1,20 @@
 """Covid module main entry"""
 import pickle
-import argparse
 import pandas as pd
 
 from covid.models import SIRV
 from covid.fetch import Dataset
+from covid import covid_argparse
 from covid.postprocessing import (plot_compartments_comparison,
                                   plot_compartment_comparison,
                                   plot_compartment, plot_compartments)
 
 
-def covid_argparse():
-    """Parse args for running compartmental model"""
-    parser = argparse.ArgumentParser(description='Plot SIRV Model')
-    parser.add_argument('-state', default='Washington')
-    parser.add_argument('-fit_compartments', default=['S', 'I', 'R', 'V'])
-    parser.add_argument('-plot_compartment', default='I')
-    parser.add_argument('-sim_days', default=100, type=int)
-    parser.add_argument('-data_days', default=100, type=int)
-    parser.add_argument('-out_file', type=str)
-    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    args = covid_argparse()
+    parser = covid_argparse()
+    args = parser.parse_args()
 
     model = SIRV()
 
